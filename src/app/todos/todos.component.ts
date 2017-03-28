da7b51234019299d22fbbff8fe7aaffc4a3b38b2
Gazda <gergely.gazda@accenture.com>
2017-03-28 11:44:43 +0200

    todo input added

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todos;
+  text;
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        text: 'Pickup kids at school'
+      },
+      {
+        text: 'Meeting with boss'
+      },
+      {
+        text: 'Dinner with wife'
+      }
+    ]
+  }
+
+  addTodo(){
+    this.todos.push({
+      text: this.text
+    });
   }
 
 }
